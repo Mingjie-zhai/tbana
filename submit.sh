3c3ddc50f11ec0a0d9a5f26aee6adad9a70d72ee
Mingjie-zhai <77083268+Mingjie-zhai@users.noreply.github.com>
2021-01-11 19:29:19 +0800

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -16,9 +16,7 @@
 # sed '1d' -i python/txt/103.txt 
 # 
 # 
-python python/sel_events.py  root/testbeam_merged_101.root event/event_10mV_101.root
-python python/sel_events.py  root/testbeam_merged_102.root event/event_10mV_102.root
-python python/sel_events.py  root/testbeam_merged_103.root event/event_10mV_103.root
-# 
+
+python python/sel_events.py  /publicfs/atlas/atlasnew/SUSY/users/zhaimingjie/testbeam/testbeamanalysis/Data/1.root /publicfs/atlas/atlasnew/SUSY/users/zhaimingjie/testbeam/code/suyu/tbana/output/1.root
 # 
 
